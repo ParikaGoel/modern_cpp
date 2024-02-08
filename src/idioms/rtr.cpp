826c9ecc44ee3ae6532a0b1d35304837783c0e1f
Parika Goel <parikagoel@gmail.com>
2024-02-08 17:04:00 +0530

    Example use of return type resolver

---
+++ src/idioms/rtr.cpp
@@ -0,0 +1,43 @@
+// RTR - Return Type Resolver
+// Also known as return type overloading
+// To deduce the type of object being assigned to 
+#include <string>
+#include <iostream>
+
+// Functions cant be overloaded based only on return type like below
+// This functionality can be achieved using RTR
+// int from_string(const std::string& s)
+// {
+//     return std::stoi(s);
+// }
+
+// float from_string(const std::string& s)
+// {
+//     return std::stof(s);
+// }
+
+class from_string{
+    public:
+        from_string(std::string str): _mstr(str){}
+
+        template<typename type>
+        operator type(){
+            if constexpr(std::is_same_v<type, int>) return std::stoi(_mstr);
+            else if (std::is_same_v<type, float>) return std::stof(_mstr);
+            else if (std::is_same_v<type, double>) return std::stod(_mstr);
+            else{
+                std::cout << "Conversion to called type not defined. Undefined behaviour\n";
+                return -1;
+            }
+        }
+    private:
+        std::string _mstr{""};
+};
+
+// int main()
+// {
+//     std::cout << int(from_string("123")) << "\n";
+//     std::cout << double(from_string("123.456")) << "\n";
+//     return 0;
+// }
+
